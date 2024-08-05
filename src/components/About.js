b6f5f73f6c3b6e0b284b8fed88b63efd35fc8b25
Sherlyne <sherlynea1994@hotmail.com>
2024-08-05 11:35:00 +0300

    Created linkend in component

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,14 @@
 import React from "react";
 
-function About() {
+function About({bio}) {
   return (
     <div id="about">
-      <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <h2>About Me </h2>
+      {/* You can read it as “if  is true bio, then (?) render <p>+ ' ✔', otherwise (:) render name”. 
+      It's called Conditional(ternary)
+      */}
+      {bio ? <p> {bio}</p>: null }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
     </div>
   );
 }
---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Links({github,linkedin}){
+  return (
+    <div>
+      <h3>Links</h3>
+      <p><a href="https://github.com/liza">https://github.com/liza</a></p>
+        <p><a href="https://www.linkedin.com/in/liza/">https://www.linkedin.com/in/liza/</a></p> 
+    </div>
+  )
+  
+}
+
+export default Links; 
